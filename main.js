8b25c8203e65ad988367f7e44e64d10371dedbfb
FelipeB1007 <102627296+FelipeB1007@users.noreply.github.com>
2024-03-10 17:58:35 -0300

    commit

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetiv02 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo01 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo02 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo03 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo04 = new Date("2025-01-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 //lembrete: o codigo  new date eu mudei para ficar apresentavelmente, o local da mudança foi 2024, q antes era 2023
